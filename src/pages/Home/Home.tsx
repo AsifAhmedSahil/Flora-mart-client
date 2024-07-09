35895b50f2c079a805edb87ab11673f88a5cce6c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-07-10 01:21:45 +0600

    hero section added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
-
+import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+      <HeroSection />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
