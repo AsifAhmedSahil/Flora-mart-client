55bb17a7b291915c68985b175ae3d19fbdc8fcbb
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-07-12 00:10:42 +0600

    allignment

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,16 +5,17 @@ import { Link } from "react-router-dom"
 
 const Products = () => {
     const {data:product,isError,isLoading} = useGetAllProductsQuery()
+    const productsToShow = product?.data.slice(0, 6);
     
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12 lg:gap-12 mt-10">
-        {
-            product?.data.map((items,index) => <ProductCart key={index} {...items}/>)
+        {productsToShow && 
+            productsToShow.map((items,index) => <ProductCart key={index} {...items}/>)
         }
     </div>
     <Link to={"/products"}>
-    <button className="btn bg-green-800 text-white hover:bg-green-700">View More</button>
+    <button className="btn bg-gradient-to-r from-green-500 to-green-800 font-bold text-white  mt-10">Explore More</button>
     </Link>
     </>
   )
